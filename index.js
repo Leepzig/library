203192fe03e796b102653633f910ec0b449bee30
andrew <neon.duncan@gmail.com>
2021-07-01 10:49:40 -0500

    typos

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const baseUrl =   "http://localhost:3000/books"
 let currentPage = 1
 const pageLimit = 5
 let books = 23
-
+document.querySelector("#current-page").innerText = `Current Page: ${currentPage}`
 
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("new-book").addEventListener("submit", Api.createNewBook)
@@ -18,6 +18,7 @@ const nextPage = () => {
   if (currentPage < books/pageLimit) {
     currentPage = currentPage + 1
     Api.fetchBooks()
+    document.querySelector("#current-page").innerText = `Current Page: ${currentPage}`
     return currentPage
   }
 }
@@ -26,6 +27,7 @@ const prevPage = () => {
   if (currentPage > 1) {
     currentPage = currentPage - 1
     Api.fetchBooks()
+    document.querySelector("#current-page").innerText = `Current Page: ${currentPage}`
     return currentPage
   }
 }
